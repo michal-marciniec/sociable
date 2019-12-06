4187625e19e09ee1480b2b0fa6295740d47a8bce
Yuriy Yakym <yakym.yuriy@gmail.com>
2019-12-06 15:26:57 +0100

    Updates

---
+++ precache-manifest.8a36542aca9b394683b8cbbaca5ef417.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d17158adb3a8a0e15e3bef3760bcb6b",
+    "url": "/sociable/index.html"
+  },
+  {
+    "revision": "ef51596dfbc1a7787e2a",
+    "url": "/sociable/static/css/main.65a62c8c.chunk.css"
+  },
+  {
+    "revision": "33957d3a4e0576df2d14",
+    "url": "/sociable/static/js/2.2a8b2b18.chunk.js"
+  },
+  {
+    "revision": "f032203ca460334c00de541c30a6078a",
+    "url": "/sociable/static/js/2.2a8b2b18.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ef51596dfbc1a7787e2a",
+    "url": "/sociable/static/js/main.eb86a19b.chunk.js"
+  },
+  {
+    "revision": "680d3f4fbaafa57e3dbe",
+    "url": "/sociable/static/js/runtime-main.7b06d53d.js"
+  }
+]);
